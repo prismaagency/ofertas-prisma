c3e5c3217c5c99d4c3be626d4a2ed0342997a46f
PRISMA AGENCY <prismaagency401@gmail.com>
2026-08-16 13:37:41 -0800

    Add files via upload

--- auth.js
+++ auth.js
@@ -6,12 +6,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "REEMPLAZAR_API_KEY",
-  authDomain: "REEMPLAZAR.firebaseapp.com",
-  projectId: "REEMPLAZAR_PROJECT_ID",
-  storageBucket: "REEMPLAZAR.firebasestorage.app",
-  messagingSenderId: "REEMPLAZAR_MESSAGING_SENDER_ID",
-  appId: "REEMPLAZAR_APP_ID"
+  apiKey: "AIzaSyCJp8kxqNLONHFNyUEsg1n09LjJaVG0GY",
+  authDomain: "prisma-agency-86c98.firebaseapp.com",
+  projectId: "prisma-agency-86c98",
+  storageBucket: "prisma-agency-86c98.firebasestorage.app",
+  messagingSenderId: "938361295336",
+  appId: "1:938361295336:web:91eaedf36823f5d8de945d"
 };
 
 const app = initializeApp(firebaseConfig);
